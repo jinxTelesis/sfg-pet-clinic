f54cdf8020e4fb325f044bb88095e9f2a7bc8091
Dre <dremon7@hotmail.com>
2019-02-11 00:44:59 -0500

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,11 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.stfpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
 
-    Owner save(Owner owner);
-
-    Set<Owner> findALl();
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,16 +1,8 @@
 package guru.springframework.sfgpetclinic.services;
 
 
-
 import guru.springframework.stfpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
+    
 }
