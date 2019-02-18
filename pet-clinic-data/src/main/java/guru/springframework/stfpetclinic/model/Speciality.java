d8022610f4cac96bea5e2be6e9171056ac60d9dc
Dre <dremon7@hotmail.com>
2019-02-18 02:56:07 -0500

    added specialities, added to vet. closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/stfpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package guru.springframework.stfpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/guru/springframework/stfpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/stfpetclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package guru.springframework.stfpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> speciality;
+
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
