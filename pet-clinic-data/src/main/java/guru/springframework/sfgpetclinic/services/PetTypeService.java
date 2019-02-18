0f0fad911cc83031e799ac87cf957fc627d5962f
Dre <dremon7@hotmail.com>
2019-02-18 03:16:04 -0500

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.stfpetclinic.model.PetType;
+
+public interface PetTypeService extends  CrudService<PetType, Long>{
+
+}
