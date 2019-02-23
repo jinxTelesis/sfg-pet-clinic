da176b71ddbbb830066ff7473442bacc639aebf5
Dre <dremon7@hotmail.com>
2019-02-22 22:59:03 -0500

    updated vet to use JPA. closes 37

--- pet-clinic-data/src/main/java/guru/springframework/stfpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/stfpetclinic/model/Speciality.java
@@ -1,7 +1,15 @@
 package guru.springframework.stfpetclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity // == entity persisted to the database
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
