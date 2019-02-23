f5f60bcdc386e50b1aef046ac53927d28c9e6a24
Dre <dremon7@hotmail.com>
2019-02-22 22:50:00 -0500

    updated owner with JPA entity mappings. closes #36

--- pet-clinic-data/src/main/java/guru/springframework/stfpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/stfpetclinic/model/Person.java
@@ -1,7 +1,15 @@
 package guru.springframework.stfpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass // prevents creation in the database
 public class Person extends BaseEntity {
+
+    @Column(name = "first_name") // hibernate
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
