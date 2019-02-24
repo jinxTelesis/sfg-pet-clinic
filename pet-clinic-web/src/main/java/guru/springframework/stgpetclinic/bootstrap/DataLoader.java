ac26ef2bb455a2c933bc627516fb7052563933bf
Dre <dremon7@hotmail.com>
2019-02-24 01:46:16 -0500

    Implemented JPA Visit Service, added loader data. closes #55

--- pet-clinic-web/src/main/java/guru/springframework/stgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/stgpetclinic/bootstrap/DataLoader.java
@@ -6,57 +6,57 @@ import org.springframework.stereotype.Component;
 @Component // spring will load into context then run method
 public class DataLoader implements CommandLineRunner {
 
-//    private final OwnerService ownerService;
-//    private final VetService vetService;
-//    private final PetTypeService petTypeService;
-//    private final SpecialitiesService specialitiesService;
+    private final OwnerService ownerService;
+    private final VetService vetService;
+    private final PetTypeService petTypeService;
+    private final SpecialitiesService specialitiesService;
 
-//    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialitiesService specialitiesService){
-//        this.ownerService = ownerService;
-//
-//        this.vetService = vetService;
-//        this.petTypeService = petTypeService;
-//        this.specialitiesService = specialitiesService;
-//    }
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialitiesService specialitiesService){
+        this.ownerService = ownerService;
+
+        this.vetService = vetService;
+        this.petTypeService = petTypeService;
+        this.specialitiesService = specialitiesService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-//        Owner owner1 = new Owner();
-//        owner1.setFirstName("Michael");
-//        owner1.setLastName("Westion");
-//
-//        ownerService.save(owner1);
-//
-//        Speciality radiology = new Speciality();
-//        radiology.setDescription("Radiology");
-//
-//        Speciality dentisitry = new Speciality();
-//        dentisitry.setDescription("dentistry");
-//
-//        Speciality surgery = new Speciality();
-//        surgery.setDescription("Surgery");
-//
-//        Owner owner2 = new Owner();
-//        owner2.setFirstName("Fiona");
-//        owner2.setLastName("Glenanne");
-//
-//        ownerService.save(owner2);
-//
-//        System.out.println("Loaded Owners....");
-//
-//        Vet vet1 = new Vet();
-//        vet1.setFirstName("Sam");
-//        vet1.setLastName("Axe");
-//
-//        vetService.save(vet1);
-//
-//        Vet vet2 = new Vet();
-//        vet2.setFirstName("Jessie");
-//        vet2.setLastName("Porter");
-//
-//        vetService.save(vet2);
-//
-//        System.out.println("Loaded Vets.....");
+        Owner owner1 = new Owner();
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Westion");
+
+        ownerService.save(owner1);
+
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+
+        Speciality dentisitry = new Speciality();
+        dentisitry.setDescription("dentistry");
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+
+        Owner owner2 = new Owner();
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded Owners....");
+
+        Vet vet1 = new Vet();
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet2);
+
+        System.out.println("Loaded Vets.....");
 
     }
 }
