9fed4c523459eeaad250971fb0291f6d58d21483
Dre <dremon7@hotmail.com>
2019-03-01 01:43:44 -0500

    added spring clinic html and a mapping to OwnerController

--- pet-clinic-web/src/test/java/guru/springframework/stgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/stgpetclinic/controllers/OwnerControllerTest.java
@@ -6,7 +6,9 @@ package guru.springframework.stgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.stfpetclinic.model.Owner;
 import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvcBuilder;
 
+import java.util.HashSet;
 import java.util.Set;
 
 //@ExtendWith(MockitoExtension.class)
@@ -20,34 +22,37 @@ class OwnerControllerTest {
 
     Set<Owner> owners;
 
-//    @BeforeEach
-//    void setUp(){
-//      owners = new HashSet<>();
-//      owners.add(Owner.builder().id(1L).build());
-//      owners.add(Owner.builder().id(2L).build());
-//
-//      mockMvc = MockMVCBuilders.standaloneSetup(controller).builder();
-//
-//    }
+    void setUp()
+    {
+        owners = new HashSet<>();
+        owners.add(Owner.builder()).id(1L).build();
+        owners.add(Owner.builder()).id(2L).builder();
 
-//    @Test
-//    void ListOwners(){
-//        when(ownerService.findAll()).thenReturn(owners);
-//
-//        mockMvc.perform(get("/owners")).andExpect(.isOk())
-//            .andExpect(view().name("owners/index"))
-//                .andExpext(model()).attribute("owners", hasSize(2));
-//
-//
-//    }
+        mocMvc = MockMvcBuilder
+                .standaloneSetup(controller)
+                .builder();
+    }
 
-//    @Test
-//    void findOwners(){
-//        mockMvc.perform(get("/owners/find"))
-//                .andExpect(status).isOk())
-//            .andExpect(view().name("notimplemented"));
-//
-//        verifyZeroInteraction(ownerService);
-//    }
+    @Test
+    void ListOwners(){
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/owners")).andExpect(.isOk())
+            .andExpect(view().name("owners/index"))
+                .andExpext(model()).attribute("owners", hasSize(2));
+
+
+    }
+
+    @Test
+    void findOwners(){
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status).isOk())
+            .andExpect(view().name("notimplemented"));
+
+        verifyZeroInteraction(ownerService);
+    }
+
+    
 
 }
